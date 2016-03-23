13b0bcadbd63cf1919c50ef8ca74a649a58554cb
HAYAMA_Kaoru <iyahaya@nifty.com>
2016-03-24 07:43:01 +0900

    Add `make package`

--- make.cmd
+++ make.cmd
@@ -6,4 +6,9 @@ if not "%1" == "" goto %1
 :install
     if exist PathToClip.exe sendto.cmd PathToClip.exe
     goto end
+
+:package
+    zip -9 PathToClip-%DATE:/=%.zip PathToClip.exe readme.md PathToClip.go sendto.cmd make.cmd
+    goto end
+
 :end
