ef7eb5adf12b7036d7961c6075161c6fc6d8711f
HAYAMA_Kaoru <iyahaya@nifty.com>
2016-03-24 07:34:43 +0900

    Support make install

--- make.cmd
+++ make.cmd
@@ -1,2 +1,9 @@
-go fmt
-go build "-ldflags=-H windowsgui"
+if not "%1" == "" goto %1
+    go fmt
+    go build "-ldflags=-H windowsgui"
+    goto end
+
+:install
+    if exist PathToClip.exe sendto.cmd PathToClip.exe
+    goto end
+:end
