5f5e4c7ecc17502fc5939b5d4407fffcb0c78991
HAYAMA_Kaoru <iyahaya@nifty.com>
2016-03-29 00:06:27 +0900

    set GOARCH=386

--- make.cmd
+++ make.cmd
@@ -1,3 +1,5 @@
+setlocal
+set "GOARCH=386"
 if not "%1" == "" goto %1
     go fmt
     go build "-ldflags=-H windowsgui"
@@ -10,5 +12,5 @@ if not "%1" == "" goto %1
 :package
     zip -9 PathToClip-%DATE:/=%.zip PathToClip.exe readme.md PathToClip.go sendto.cmd make.cmd
     goto end
-
 :end
+endlocal
